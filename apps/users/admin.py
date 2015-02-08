5eac36ed5f7ca1eab0edf4731fdf5f6832b4bd75
Thom <tomdagz@gmail.com>
2015-02-08 09:04:58 -0600

    added suit and admin for users

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from .models import User
+from .actions import export_as_excel
 # Register your models here.
 
-admin.site.register(User)
+# @admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username','first_name', 'last_name',)
+    search_fields = ('username', 'email',)
+    list_filter = ('is_superuser',)
+    ordering = ('username',)
+    filter_horizontal = ('groups', 'user_permissions',)
+    actions = [export_as_excel]
+    fieldsets = (
+        ('User', {'fields':('username','password')}),
+        ('Personal info', {'fields':('first_name', 'last_name', 'email', 'avatar',)}),
+        ('Permissions', {'fields':('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions',)})
+    )
+admin.site.register(User, UserAdmin)
